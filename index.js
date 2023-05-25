9d767c6980231c280150676c55cfe96832c73114
Kenneth Muuli <kenneth.muuli@gmail.com>
2023-05-25 15:01:14 +0300

    added css

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
